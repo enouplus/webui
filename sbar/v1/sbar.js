98b4975330b5cc0b36e7f80bf22bbf161987ffdf
enouplus <62318261+enouplus@users.noreply.github.com>
2020-10-12 16:34:23 +0530

    Delete sbar.js

--- sbar/v1/sbar.js
+++
@@ -1,8 +0,0 @@
-function sbar(x)
-{
-    var sbar=document.getElementById(x.getAttribute("data-sbar"));
-    var sbar_o=document.getElementById(x.getAttribute("data-sbar_o"));
-    sbar.classList.toggle("sbar_block");
-    sbar_o.classList.toggle("sbar_block");
-
-}
