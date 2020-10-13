cfcef80b26ea47ad4226c67b89e04895b033d873
enouplus <62318261+enouplus@users.noreply.github.com>
2020-10-13 12:40:29 +0530

    Update sbar.js

--- sbar.js
+++ sbar.js
@@ -1,8 +1,1 @@
-function sbar(x)
-{
-    var sbar=document.getElementById(x.getAttribute("data-sbar"));
-    var sbar_o=document.getElementById(x.getAttribute("data-sbar_o"));
-    sbar.classList.toggle("sbar_block");
-    sbar_o.classList.toggle("sbar_block");
-
-}
+function sbar_droplinks_title(t){document.getElementById(t.getAttribute("data-sbar_droplinks")).classList.toggle("sbar_droplinks_shown"),t.classList.toggle("sbar_droplinks_title_dropped")}function sbar(t){var s=document.getElementById(t.getAttribute("data-sbar")),e=document.getElementById(t.getAttribute("data-sbar_o"));s.classList.toggle("sbar_shown"),e.classList.toggle("sbar_shown")}
